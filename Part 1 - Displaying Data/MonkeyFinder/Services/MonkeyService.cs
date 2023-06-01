b57472fdaeafa934f7ca1c6819eb2d2b210ed09d
Seth Faerber <seth.faerber@ramseysolutions.com>
2023-06-01 10:23:30 -0500

    my changes added

--- Part 1 - Displaying Data/MonkeyFinder/Services/MonkeyService.cs
+++ Part 1 - Displaying Data/MonkeyFinder/Services/MonkeyService.cs
@@ -1,5 +1,32 @@
-﻿namespace MonkeyFinder.Services;
+﻿using System.Net.Http.Json;
+
+namespace MonkeyFinder.Services;
 
 public class MonkeyService
 {
+    // Type   variable initialization
+    HttpClient httpClient;
+    
+    // constructor because same name as initialized class()
+    public MonkeyService()
+    {
+        this.httpClient = new HttpClient();
+    }
+    
+    List<Monkey> monkeyList = new ();
+    
+    public async Task<List<Monkey>> GetMonkeys()
+    {
+        if (monkeyList?.Count > 0)
+            return monkeyList;
+        
+        var response = await httpClient.GetAsync("https://www.montemagno.com/monkeys.json");
+
+        if (response.IsSuccessStatusCode)
+        {
+            monkeyList = await response.Content.ReadFromJsonAsync<List<Monkey>>();
+        }
+        
+        return monkeyList;
+    }
 }
--- Part 1 - Displaying Data/MonkeyFinder/View/MainPage.xaml.cs
+++ Part 1 - Displaying Data/MonkeyFinder/View/MainPage.xaml.cs
@@ -2,9 +2,9 @@
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
+	public MainPage(MonkeysViewModel viewModel)
 	{
 		InitializeComponent();
+		BindingContext = viewModel;
 	}
 }
-
